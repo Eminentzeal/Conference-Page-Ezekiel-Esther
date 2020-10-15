55e78c8e030d925c15b56541c821c8652f2c86b5
Ezekiel Ogunniwa <ogunniwaezekiel@gmail.com>
2020-10-15 12:14:47 +0100

    finalizing homepage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,12 @@
 const purchaseBtn = document.querySelector('.button')
 purchaseBtn.addEventListener('click',function() {
-
     let name = document.getElementById('yourName').value;
     let email = document.getElementById('yourEmail').value;
     let noOfPasses = document.querySelector('.select').value;
     let comments = document.querySelector('.comments').value;
-    
-
     if (name == '' || email == '' || noOfPasses == '' || comments == '') {
-        alert('all fields required');
-        return;
+        console.log('all fields required'); return;
     }
     else {
-        alert('Purchase Successful');
+        console.log('Purchase Successful'); return;
     }})
